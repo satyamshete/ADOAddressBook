498da18096e1f894a811b690e81e18e154ce7521
satyamshete <shetesatyam99@gmail.com>
2022-10-02 02:37:28 +0530

    [Satyam] Disply Contacts

---
+++ ADO.NetAddressBook/AddressBookADO.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ADO.NetAddressBook
+{
+    internal class AddressBookADO
+    {
+        public static string connectionString = "Data Source=(localdb)\\ProjectModels;Initial Catalog=AddressBookService;";
+        Contact person = new Contact();
+
+        public void GetContactDetails()
+        {
+
+            SqlConnection sqlConnect = new SqlConnection(connectionString);
+            try
+            {
+                using (sqlConnect)
+                {
+                    sqlConnect.Open();
+
+                    SqlCommand com = new SqlCommand("GetContactDetails", sqlConnect);
+                    //query, sqlconnection
+                    com.CommandType = CommandType.StoredProcedure;
+                    SqlDataReader dr = com.ExecuteReader();
+
+                    if (dr.HasRows)
+                    {
+                        Console.WriteLine("\n Person contacts Contents : \n");
+                        Console.WriteLine();
+                        while (dr.Read())
+                        {
+                            person.FirstName = dr.GetString(0);
+                            person.LastName = dr.GetString(1);
+                            person.Address = dr.GetString(2);
+                            person.City = dr.GetString(3);
+                            person.State = dr.GetString(4);
+                            person.ZipCode = dr.GetInt32(5);
+                            person.PhoneNumber = dr.GetInt64(6);
+                            person.EmailId = dr.GetString(7);
+                            person.Type = dr.GetString(8);
+
+
+                            Console.WriteLine(" {0} {1} {2} {3} {4} {5} {6} {7} {8}", person.FirstName, person.LastName, person.Address, person.City, person.State, person.ZipCode, person.PhoneNumber, person.EmailId, person.Type);
+                        }
+                    }
+                    dr.Close();
+                }
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e.Message);
+
+            }
+            finally
+            {
+                sqlConnect.Close();
+            }
+        }
+    }
+}
---
+++ ADO.NetAddressBook/Program.cs
@@ -0,0 +1,30 @@
+﻿namespace ADO.NetAddressBook
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(" Welcome to contact management program");
+            AddressBookADO Book = new AddressBookADO();
+
+            ShowOptions();
+
+            void ShowOptions()
+            {
+                Console.Write("\n Select Option : 1.Display Contacts  \n ");
+                Console.WriteLine();
+                int option = int.Parse(Console.ReadLine());
+                switch (option)
+                {
+                    case 1:
+                        Book.GetContactDetails();
+                        ShowOptions();
+                        break;
+                    default:
+                        Console.WriteLine("Enter correct choice");
+                        break;
+                }
+            }
+        }
+    }
+}
